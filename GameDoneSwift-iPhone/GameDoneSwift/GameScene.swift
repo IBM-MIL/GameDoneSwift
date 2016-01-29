6ce92b006c326b0657c5bb6c943b39bfd71fb119
EVAN S. COMPTON <escompto@us.ibm.com>
2016-01-29 16:35:47 -0600

    Long live the king

--- GameDoneSwift-iPhone/GameDoneSwift/GameScene.swift
+++ GameDoneSwift-iPhone/GameDoneSwift/GameScene.swift
@@ -9,36 +9,34 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    
+    let kingSpriteName = "king"
+    let groundSpriteName = "ground"
+    let backgroundSpriteName = "background"
+    
+    var king: SKSpriteNode!
+    var ground: SKSpriteNode!
+    var background: SKSpriteNode!
+    
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+        guard let king = self.childNodeWithName(kingSpriteName) as? SKSpriteNode,
+            let ground = self.childNodeWithName(groundSpriteName) as? SKSpriteNode,
+            let background = self.childNodeWithName(backgroundSpriteName) as? SKSpriteNode
+            else {
+                print("Sprites not found.")
+                return
+        }
         
-        self.addChild(myLabel)
+        self.king = king
+        self.ground = ground
+        self.background = background
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
-        
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"unicorn")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        /* Called when a touch begins */
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
